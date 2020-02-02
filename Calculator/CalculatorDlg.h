b2e0531b755cca2215e8820e5425bb5c12e6375c
Тимофей Киневич <thetim182001@mail.ru>
2020-02-02 16:06:34 +0300

    Добавьте файлы проекта.

---
+++ Calculator/CalculatorDlg.h
@@ -0,0 +1,34 @@
+﻿
+// CalculatorDlg.h: файл заголовка
+//
+
+#pragma once
+
+
+// Диалоговое окно CCalculatorDlg
+class CCalculatorDlg : public CDialogEx
+{
+// Создание
+public:
+	CCalculatorDlg(CWnd* pParent = nullptr);	// стандартный конструктор
+
+// Данные диалогового окна
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CALCULATOR_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// поддержка DDX/DDV
+
+
+// Реализация
+protected:
+	HICON m_hIcon;
+
+	// Созданные функции схемы сообщений
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
